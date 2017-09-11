5e5bcfe6f03a5fd3bf9644c8eab7417639072338
kartozia <inga.kartoziya@gmail.com>
2017-09-11 21:39:34 +0300

    final

--- для Дениса/homework_1/task_1.py
+++ для Дениса/homework_1/task_1.py
@@ -1,12 +1,9 @@
 #Картозия,домашнее здание №1, задание 1
 def fibonacci(n):
-    memo = {}
-    a,b = 1,1
-    if n not in memo:
-        for i in range(n-1):
-            a,b = b, a+b
-            memo[n] = a
-    return memo[n]
+    a,b = 0,1
+    for i in range(n):
+        a,b = b, a+b
+    return a
 
 #print(fibonacci(10))
 
