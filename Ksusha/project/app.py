811904e38bea506ed611e5e27374eaea70e9e0df
kartozia <inga.kartoziya@gmail.com>
2017-10-18 20:55:06 +0300

    project update

--- Ksusha/project/app.py
+++ Ksusha/project/app.py
@@ -10,8 +10,8 @@ from nltk.corpus import stopwords
 from flask import Flask, request, render_template
 from math import log
 
+m = Mystem()
 def lemmatize_me(text):
-    m = Mystem()
     lemma = m.lemmatize(text)
 
     #let's delete punctuation symbols
@@ -58,13 +58,14 @@ def extract_info(text, key):
     qf = compute_tf(key, lemmatize_me(content))
     return url, title, content, dl, qf
 
-def metrics(result, N='24', avdl='289.2083333333333', k1='2.0', b='0.75'):
+def metrics(result, N=1000, avdl=253.233, k1=2.0, b=0.75):
     output = {}
     for key, value in result.items():
         n = len(value)
         for file in value:
-            f = open('./text/' + file, 'r', encoding='utf-8')
+            f = open('./news/' + file, 'r', encoding='utf-8')
             text = f.read()
+            f.close()
             url, title, content, dl, qf = extract_info(text, key)
             metric = score_BM25(n, qf, N, dl, avdl)
             link = '<a href="' + url + '">' + title + '</a>'
@@ -72,7 +73,6 @@ def metrics(result, N='24', avdl='289.2083333333333', k1='2.0', b='0.75'):
                 output[link] = metric
             else:
                 output[link] += metric
-            f.close()
 
     return sorted(output, key=output.get, reverse=True)[:10]        
     #for key in sorted(output, key=output.get, reverse=True)[:10]:
@@ -84,13 +84,14 @@ def search(query):
     json_data=open('reversed_index.json', 'r', encoding='utf-8')
     j = json_data.read()
     data = json.loads(j)
+    json_data.close()
     lem_query = lemmatize_me(query)
     result = defaultdict(set)
     for key, value in data.items():
         for word in lem_query:
             if word == key:
                 result[word] = set(value)
-    output = metrics(result, N=24, avdl=289.2083333333333, k1=2.0, b=0.75)
+    output = metrics(result)
     return output
 
 app = Flask(__name__)
